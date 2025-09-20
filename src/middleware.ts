cd6a85dd6982f3e824bc164bee837e4214bf1a80
jaafar2000 <jaafar200021@gmail.com>
2025-09-20 22:58:09 +0400

    .

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,17 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/api/webhooks(.*)", "/sign-up(.*)"]);
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/api/webhooks(.*)",
+]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
     await auth.protect();
   }
 });
-
 
 export const config = {
   matcher: [
